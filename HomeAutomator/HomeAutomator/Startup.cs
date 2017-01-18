b095eafd7a47855cd71938eb5f89124a39c0895b
R. J. Lewis III <rjlewis@resfor.caci.com>
2017-01-18 15:25:38 -0500

    Initial commit

---
+++ HomeAutomator/HomeAutomator/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HomeAutomator.Startup))]
+namespace HomeAutomator
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
